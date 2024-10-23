c5653543b4972e8b4cac75797dd68281816030d8
Franklin Not <franklinnot@outlook.com>
2024-10-22 19:29:05 -0500

    Creacion de proyecto

---
+++ BuccalMedic/Capa Entidad/Empleado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad
+{
+    public class Empleado : Usuario
+    {
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
