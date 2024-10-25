f1490f95e681107a1e533c758b60c7eb3ff4a4a9
Flavio Zavaleta <zflavioalberto@gmail.com>
2024-10-24 23:29:05 -0500

    Interfaz Evaluaciones Completa

--- BuccalMedic/CapaPresentacion/form_login.cs
+++ BuccalMedic/CapaPresentacion/form_login.cs
@@ -18,6 +18,9 @@ namespace CapaPresentacion
         public form_login()
         {
             InitializeComponent();
+            //llamar al formulario evaluaciones
+            form_evaluaciones evaluaciones = new form_evaluaciones();
+            evaluaciones.ShowDialog();
         }
 
         private void form_login_Load(object sender, EventArgs e)
