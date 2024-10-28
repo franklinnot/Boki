a8b6f2a57ae4ccf648a7c95ec68bd92cff676df2
Belen <anabelensanchezboy@gmail.com>
2024-10-27 19:37:47 -0500

    Cambio nombre logo

--- BuccalMedic/CapaPresentacion/form_login.Designer.cs
+++ BuccalMedic/CapaPresentacion/form_login.Designer.cs
@@ -42,7 +42,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::CapaPresentacion.Properties.Resources.loguito;
+            this.pictureBox1.Image = global::CapaPresentacion.Properties.Resources.BuccalMedic;
             this.pictureBox1.Location = new System.Drawing.Point(244, 85);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(275, 85);
--- BuccalMedic/CapaPresentacion/form_recepcion.Designer.cs
+++ BuccalMedic/CapaPresentacion/form_recepcion.Designer.cs
@@ -70,7 +70,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::CapaPresentacion.Properties.Resources.loguito;
+            this.pictureBox1.Image = global::CapaPresentacion.Properties.Resources.BuccalMedic;
             this.pictureBox1.Location = new System.Drawing.Point(185, 17);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(182, 68);
