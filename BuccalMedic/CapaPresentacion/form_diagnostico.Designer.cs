c9ffd8732a577b7dca8855da6c295e5a2359dca1
YuleisyQuipuzcoa22 <yuleisyquipuzcoa@gmail.com>
2024-10-29 00:42:31 -0500

    Diagnostico
    cita de consulta

--- BuccalMedic/CapaPresentacion/form_diagnostico.Designer.cs
+++ BuccalMedic/CapaPresentacion/form_diagnostico.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form_diagnostico));
             this.panel1 = new System.Windows.Forms.Panel();
             this.txtrecomendacionesd = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
@@ -41,14 +42,20 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Controls.Add(this.txtrecomendacionesd);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.txt_resultadod);
@@ -62,79 +69,78 @@
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(9, 10);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Location = new System.Drawing.Point(-4, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(584, 332);
+            this.panel1.Size = new System.Drawing.Size(812, 442);
             this.panel1.TabIndex = 0;
             // 
             // txtrecomendacionesd
             // 
-            this.txtrecomendacionesd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(250)))), ((int)(((byte)(243)))));
-            this.txtrecomendacionesd.Location = new System.Drawing.Point(277, 104);
-            this.txtrecomendacionesd.Margin = new System.Windows.Forms.Padding(2);
+            this.txtrecomendacionesd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtrecomendacionesd.Location = new System.Drawing.Point(389, 155);
+            this.txtrecomendacionesd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtrecomendacionesd.Multiline = true;
             this.txtrecomendacionesd.Name = "txtrecomendacionesd";
-            this.txtrecomendacionesd.Size = new System.Drawing.Size(173, 82);
+            this.txtrecomendacionesd.Size = new System.Drawing.Size(369, 99);
             this.txtrecomendacionesd.TabIndex = 15;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Britannic Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(29, 200);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(61, 275);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(82, 16);
+            this.label6.Size = new System.Drawing.Size(99, 18);
             this.label6.TabIndex = 14;
             this.label6.Text = "Tratamiento";
             // 
             // txt_resultadod
             // 
-            this.txt_resultadod.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(250)))), ((int)(((byte)(243)))));
-            this.txt_resultadod.Location = new System.Drawing.Point(32, 104);
-            this.txt_resultadod.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_resultadod.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txt_resultadod.Location = new System.Drawing.Point(63, 155);
+            this.txt_resultadod.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_resultadod.Multiline = true;
             this.txt_resultadod.Name = "txt_resultadod";
-            this.txt_resultadod.Size = new System.Drawing.Size(173, 81);
+            this.txt_resultadod.Size = new System.Drawing.Size(266, 99);
             this.txt_resultadod.TabIndex = 13;
             // 
             // txtNombre
             // 
-            this.txtNombre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(250)))), ((int)(((byte)(243)))));
-            this.txtNombre.Location = new System.Drawing.Point(393, 55);
-            this.txtNombre.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNombre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtNombre.Location = new System.Drawing.Point(554, 96);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(154, 20);
+            this.txtNombre.Size = new System.Drawing.Size(204, 22);
             this.txtNombre.TabIndex = 12;
             // 
             // txtDni
             // 
-            this.txtDni.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(250)))), ((int)(((byte)(243)))));
-            this.txtDni.Location = new System.Drawing.Point(68, 56);
-            this.txtDni.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDni.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtDni.Location = new System.Drawing.Point(112, 96);
+            this.txtDni.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtDni.Name = "txtDni";
-            this.txtDni.Size = new System.Drawing.Size(96, 20);
+            this.txtDni.Size = new System.Drawing.Size(127, 22);
             this.txtDni.TabIndex = 11;
             // 
             // chlbtratamientos
             // 
-            this.chlbtratamientos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(250)))), ((int)(((byte)(243)))));
+            this.chlbtratamientos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.chlbtratamientos.FormattingEnabled = true;
-            this.chlbtratamientos.Location = new System.Drawing.Point(32, 218);
-            this.chlbtratamientos.Margin = new System.Windows.Forms.Padding(2);
+            this.chlbtratamientos.Location = new System.Drawing.Point(64, 295);
+            this.chlbtratamientos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.chlbtratamientos.Name = "chlbtratamientos";
-            this.chlbtratamientos.Size = new System.Drawing.Size(173, 94);
+            this.chlbtratamientos.Size = new System.Drawing.Size(229, 106);
             this.chlbtratamientos.TabIndex = 10;
             // 
             // btn_registrardiagnosticod
             // 
-            this.btn_registrardiagnosticod.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(241)))), ((int)(((byte)(244)))));
-            this.btn_registrardiagnosticod.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_registrardiagnosticod.Location = new System.Drawing.Point(302, 236);
-            this.btn_registrardiagnosticod.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_registrardiagnosticod.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(169)))), ((int)(((byte)(234)))));
+            this.btn_registrardiagnosticod.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_registrardiagnosticod.Location = new System.Drawing.Point(623, 351);
+            this.btn_registrardiagnosticod.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_registrardiagnosticod.Name = "btn_registrardiagnosticod";
-            this.btn_registrardiagnosticod.Size = new System.Drawing.Size(117, 58);
+            this.btn_registrardiagnosticod.Size = new System.Drawing.Size(135, 50);
             this.btn_registrardiagnosticod.TabIndex = 9;
             this.btn_registrardiagnosticod.Text = "Registrar";
             this.btn_registrardiagnosticod.UseVisualStyleBackColor = false;
@@ -143,82 +149,101 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Britannic Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(274, 86);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(386, 133);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(128, 16);
+            this.label5.Size = new System.Drawing.Size(151, 18);
             this.label5.TabIndex = 7;
             this.label5.Text = "Recomendaciones: ";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Britannic Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(208, 11);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(60)))), ((int)(((byte)(44)))));
+            this.label4.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(225)))), ((int)(((byte)(238)))));
+            this.label4.Location = new System.Drawing.Point(218, 9);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(155, 27);
+            this.label4.Size = new System.Drawing.Size(353, 41);
             this.label4.TabIndex = 6;
-            this.label4.Text = "Cita Consulta";
+            this.label4.Text = "CITA DE CONSULTA";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Britannic Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(268, 55);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(386, 95);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(135, 16);
+            this.label1.Size = new System.Drawing.Size(162, 18);
             this.label1.TabIndex = 5;
             this.label1.Text = "Nombre y Apellidos: ";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Britannic Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(29, 86);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(60, 133);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 16);
+            this.label3.Size = new System.Drawing.Size(100, 18);
             this.label3.TabIndex = 4;
             this.label3.Text = "Resultados: ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Britannic Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(29, 56);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 96);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 16);
+            this.label2.Size = new System.Drawing.Size(47, 18);
             this.label2.TabIndex = 2;
             this.label2.Text = "DNI: ";
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(623, 9);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(172, 51);
+            this.pictureBox2.TabIndex = 22;
+            this.pictureBox2.TabStop = false;
+            // 
             // pictureBox1
             // 
-            this.pictureBox1.BackgroundImage = global::CapaPresentacion.Properties.Resources.BuccalMedic;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(446, 2);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(60)))), ((int)(((byte)(44)))));
+            this.pictureBox1.Location = new System.Drawing.Point(193, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(136, 45);
-            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.Size = new System.Drawing.Size(401, 63);
+            this.pictureBox1.TabIndex = 23;
             this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(64, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(99, 88);
+            this.pictureBox3.TabIndex = 24;
+            this.pictureBox3.TabStop = false;
             // 
             // form_diagnostico
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(602, 352);
+            this.ClientSize = new System.Drawing.Size(803, 433);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "form_diagnostico";
             this.Text = "form_diagnostico";
             this.Load += new System.EventHandler(this.form_diagnostico_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -226,7 +251,6 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label1;
@@ -239,5 +263,8 @@
         private System.Windows.Forms.TextBox txtNombre;
         private System.Windows.Forms.TextBox txtDni;
         public System.Windows.Forms.CheckedListBox chlbtratamientos;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
