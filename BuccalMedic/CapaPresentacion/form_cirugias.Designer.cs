02dcc7553f4f7c47cd971dfdd467b2fb642f8e97
YuleisyQuipuzcoa22 <yuleisyquipuzcoa@gmail.com>
2024-10-28 23:53:40 -0500

    cirugias
    historial de tratamientos

--- BuccalMedic/CapaPresentacion/form_cirugias.Designer.cs
+++ BuccalMedic/CapaPresentacion/form_cirugias.Designer.cs
@@ -28,11 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form_cirugias));
             this.panel_cirugias = new System.Windows.Forms.Panel();
-            this.cirugias_btn_regresar = new System.Windows.Forms.Button();
             this.cirugia_btn_Limpiar = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.cirugias_dgv = new System.Windows.Forms.DataGridView();
             this.IdCita = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -43,21 +42,24 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.panel_cirugias.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.cirugias_dgv)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // panel_cirugias
             // 
             this.panel_cirugias.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.panel_cirugias.BackColor = System.Drawing.Color.White;
-            this.panel_cirugias.Controls.Add(this.cirugias_btn_regresar);
-            this.panel_cirugias.Controls.Add(this.cirugia_btn_Limpiar);
+            this.panel_cirugias.Controls.Add(this.label5);
             this.panel_cirugias.Controls.Add(this.pictureBox1);
+            this.panel_cirugias.Controls.Add(this.label1);
+            this.panel_cirugias.Controls.Add(this.cirugia_btn_Limpiar);
             this.panel_cirugias.Controls.Add(this.cirugias_dgv);
             this.panel_cirugias.Controls.Add(this.cirugia_codigocita);
             this.panel_cirugias.Controls.Add(this.cirugia_cmbox_paciente);
@@ -65,60 +67,44 @@
             this.panel_cirugias.Controls.Add(this.label4);
             this.panel_cirugias.Controls.Add(this.label3);
             this.panel_cirugias.Controls.Add(this.label2);
-            this.panel_cirugias.Controls.Add(this.label1);
-            this.panel_cirugias.Controls.Add(this.pictureBox2);
+            this.panel_cirugias.Controls.Add(this.pictureBox3);
             this.panel_cirugias.Location = new System.Drawing.Point(-4, -3);
             this.panel_cirugias.Name = "panel_cirugias";
             this.panel_cirugias.Size = new System.Drawing.Size(810, 539);
             this.panel_cirugias.TabIndex = 2;
             // 
-            // cirugias_btn_regresar
-            // 
-            this.cirugias_btn_regresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(180)))), ((int)(((byte)(171)))));
-            this.cirugias_btn_regresar.Location = new System.Drawing.Point(17, 464);
-            this.cirugias_btn_regresar.Margin = new System.Windows.Forms.Padding(4);
-            this.cirugias_btn_regresar.Name = "cirugias_btn_regresar";
-            this.cirugias_btn_regresar.Size = new System.Drawing.Size(89, 44);
-            this.cirugias_btn_regresar.TabIndex = 17;
-            this.cirugias_btn_regresar.Text = "<--";
-            this.cirugias_btn_regresar.UseVisualStyleBackColor = false;
-            // 
             // cirugia_btn_Limpiar
             // 
-            this.cirugia_btn_Limpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(241)))), ((int)(((byte)(244)))));
+            this.cirugia_btn_Limpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(225)))), ((int)(((byte)(238)))));
             this.cirugia_btn_Limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cirugia_btn_Limpiar.Location = new System.Drawing.Point(661, 457);
+            this.cirugia_btn_Limpiar.Location = new System.Drawing.Point(642, 464);
             this.cirugia_btn_Limpiar.Name = "cirugia_btn_Limpiar";
-            this.cirugia_btn_Limpiar.Size = new System.Drawing.Size(133, 51);
+            this.cirugia_btn_Limpiar.Size = new System.Drawing.Size(133, 45);
             this.cirugia_btn_Limpiar.TabIndex = 11;
             this.cirugia_btn_Limpiar.Text = "Limpiar";
             this.cirugia_btn_Limpiar.UseVisualStyleBackColor = false;
             this.cirugia_btn_Limpiar.Click += new System.EventHandler(this.cirugia_btn_Limpiar_Click);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(622, 17);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(174, 44);
-            this.pictureBox1.TabIndex = 8;
-            this.pictureBox1.TabStop = false;
-            // 
             // cirugias_dgv
             // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(169)))), ((int)(((byte)(234)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.cirugias_dgv.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.cirugias_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.cirugias_dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.IdCita,
             this.Nombre,
             this.Fecha});
-            this.cirugias_dgv.Location = new System.Drawing.Point(294, 151);
+            this.cirugias_dgv.Location = new System.Drawing.Point(285, 128);
             this.cirugias_dgv.Name = "cirugias_dgv";
             this.cirugias_dgv.RowHeadersWidth = 51;
             this.cirugias_dgv.RowTemplate.Height = 24;
-            this.cirugias_dgv.Size = new System.Drawing.Size(502, 271);
+            this.cirugias_dgv.Size = new System.Drawing.Size(490, 271);
             this.cirugias_dgv.TabIndex = 7;
             // 
             // IdCita
@@ -199,27 +185,50 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Selecciona Fecha:";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(266, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(259, 28);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Historial de tratamientos";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(602, 37);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(173, 44);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(285, 30);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(286, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(258, 54);
-            this.label1.TabIndex = 0;
+            this.label1.Size = new System.Drawing.Size(226, 48);
+            this.label1.TabIndex = 20;
             this.label1.Text = "CIRUGÍAS";
             // 
-            // pictureBox2
+            // pictureBox3
             // 
-            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
-            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(807, 103);
-            this.pictureBox2.TabIndex = 10;
-            this.pictureBox2.TabStop = false;
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(-149, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(978, 585);
+            this.pictureBox3.TabIndex = 22;
+            this.pictureBox3.TabStop = false;
             // 
             // form_cirugias
             // 
@@ -232,9 +241,9 @@
             this.Load += new System.EventHandler(this.form_cirugias_Load);
             this.panel_cirugias.ResumeLayout(false);
             this.panel_cirugias.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.cirugias_dgv)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -243,7 +252,6 @@
 
         private System.Windows.Forms.Panel panel_cirugias;
         private System.Windows.Forms.Button cirugia_btn_Limpiar;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.DataGridView cirugias_dgv;
         private System.Windows.Forms.ComboBox cirugia_codigocita;
         private System.Windows.Forms.ComboBox cirugia_cmbox_paciente;
@@ -251,11 +259,12 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.DataGridViewTextBoxColumn IdCita;
         private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
         private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
-        private System.Windows.Forms.Button cirugias_btn_regresar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
