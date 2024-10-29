b7b5a1168b7b234cc36bd0da088504bb6b35139e
YuleisyQuipuzcoa22 <yuleisyquipuzcoa@gmail.com>
2024-10-28 23:32:28 -0500

    selección_doctor

--- BuccalMedic/CapaPresentacion/form_seleccionDoc.Designer.cs
+++ BuccalMedic/CapaPresentacion/form_seleccionDoc.Designer.cs
@@ -37,14 +37,19 @@
             this.btn_citasd = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.pictureBox4);
             this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Controls.Add(this.btn_tratamientod);
             this.panel1.Controls.Add(this.btn_intervencionesd);
@@ -52,31 +57,33 @@
             this.panel1.Controls.Add(this.btn_citasd);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(9, 10);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Location = new System.Drawing.Point(1, -2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(582, 346);
+            this.panel1.Size = new System.Drawing.Size(803, 461);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackgroundImage = global::CapaPresentacion.Properties.Resources.OIP;
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox2.Location = new System.Drawing.Point(213, 80);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(284, 98);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(294, 231);
+            this.pictureBox2.Size = new System.Drawing.Size(392, 284);
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
             // 
             // btn_tratamientod
             // 
-            this.btn_tratamientod.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(180)))), ((int)(((byte)(171)))));
-            this.btn_tratamientod.Font = new System.Drawing.Font("Britannic Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_tratamientod.Location = new System.Drawing.Point(40, 268);
-            this.btn_tratamientod.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_tratamientod.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(52)))), ((int)(((byte)(148)))));
+            this.btn_tratamientod.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_tratamientod.ForeColor = System.Drawing.Color.White;
+            this.btn_tratamientod.Location = new System.Drawing.Point(53, 330);
+            this.btn_tratamientod.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_tratamientod.Name = "btn_tratamientod";
-            this.btn_tratamientod.Size = new System.Drawing.Size(117, 58);
+            this.btn_tratamientod.Size = new System.Drawing.Size(179, 71);
             this.btn_tratamientod.TabIndex = 5;
             this.btn_tratamientod.Text = "Servicios";
             this.btn_tratamientod.UseVisualStyleBackColor = false;
@@ -84,12 +91,13 @@
             // 
             // btn_intervencionesd
             // 
-            this.btn_intervencionesd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(180)))), ((int)(((byte)(171)))));
-            this.btn_intervencionesd.Font = new System.Drawing.Font("Britannic Bold", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_intervencionesd.Location = new System.Drawing.Point(40, 201);
-            this.btn_intervencionesd.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_intervencionesd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(103)))), ((int)(((byte)(196)))));
+            this.btn_intervencionesd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_intervencionesd.ForeColor = System.Drawing.Color.White;
+            this.btn_intervencionesd.Location = new System.Drawing.Point(53, 247);
+            this.btn_intervencionesd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_intervencionesd.Name = "btn_intervencionesd";
-            this.btn_intervencionesd.Size = new System.Drawing.Size(117, 58);
+            this.btn_intervencionesd.Size = new System.Drawing.Size(179, 71);
             this.btn_intervencionesd.TabIndex = 4;
             this.btn_intervencionesd.Text = "Historial de Tratamientos";
             this.btn_intervencionesd.UseVisualStyleBackColor = false;
@@ -97,12 +105,12 @@
             // 
             // btn_evaluacionesd
             // 
-            this.btn_evaluacionesd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(180)))), ((int)(((byte)(171)))));
-            this.btn_evaluacionesd.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_evaluacionesd.Location = new System.Drawing.Point(40, 132);
-            this.btn_evaluacionesd.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_evaluacionesd.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btn_evaluacionesd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_evaluacionesd.Location = new System.Drawing.Point(53, 162);
+            this.btn_evaluacionesd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_evaluacionesd.Name = "btn_evaluacionesd";
-            this.btn_evaluacionesd.Size = new System.Drawing.Size(117, 58);
+            this.btn_evaluacionesd.Size = new System.Drawing.Size(179, 71);
             this.btn_evaluacionesd.TabIndex = 3;
             this.btn_evaluacionesd.Text = "Historial de Consultas";
             this.btn_evaluacionesd.UseVisualStyleBackColor = false;
@@ -110,12 +118,12 @@
             // 
             // btn_citasd
             // 
-            this.btn_citasd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(180)))), ((int)(((byte)(171)))));
-            this.btn_citasd.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_citasd.Location = new System.Drawing.Point(40, 65);
-            this.btn_citasd.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_citasd.BackColor = System.Drawing.Color.LightBlue;
+            this.btn_citasd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_citasd.Location = new System.Drawing.Point(53, 80);
+            this.btn_citasd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btn_citasd.Name = "btn_citasd";
-            this.btn_citasd.Size = new System.Drawing.Size(117, 58);
+            this.btn_citasd.Size = new System.Drawing.Size(179, 71);
             this.btn_citasd.TabIndex = 2;
             this.btn_citasd.Text = "Ver Citas";
             this.btn_citasd.UseVisualStyleBackColor = false;
@@ -123,41 +131,65 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackgroundImage = global::CapaPresentacion.Properties.Resources.BuccalMedic;
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox1.ErrorImage = null;
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(413, 2);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Location = new System.Drawing.Point(553, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(166, 57);
+            this.pictureBox1.Size = new System.Drawing.Size(221, 70);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Britannic Bold", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(35, 19);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(46, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(260, 30);
+            this.label1.Size = new System.Drawing.Size(317, 37);
             this.label1.TabIndex = 0;
-            this.label1.Text = "¿A donde quieres ir?";
+            this.label1.Text = "¿A dónde quieres ir?";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(-10, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(813, 458);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(630, 330);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 84);
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
             // 
             // form_seleccionDoc
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "form_seleccionDoc";
             this.Text = "form_seleccionDoc";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -172,5 +204,7 @@
         private System.Windows.Forms.Button btn_evaluacionesd;
         private System.Windows.Forms.Button btn_citasd;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
