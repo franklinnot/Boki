34a596bb5540a04806e969c5cc158241adfebc3c
YuleisyQuipuzcoa22 <yuleisyquipuzcoa@gmail.com>
2024-10-28 23:15:59 -0500

    tratamientos

--- BuccalMedic/CapaPresentacion/form_tratamientos.Designer.cs
+++ BuccalMedic/CapaPresentacion/form_tratamientos.Designer.cs
@@ -35,19 +35,19 @@
             this.tratamiento_tbx_Descripcion = new System.Windows.Forms.TextBox();
             this.tratamiento_tbx_Nombre_Tratamiento = new System.Windows.Forms.TextBox();
             this.tratamiento_btn_Registrar = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tratamientos_dgv = new System.Windows.Forms.DataGridView();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.idTratamiento = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel_tratamientos.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tratamientos_dgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -55,18 +55,18 @@
             // 
             this.panel_tratamientos.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.panel_tratamientos.BackColor = System.Drawing.Color.White;
+            this.panel_tratamientos.Controls.Add(this.label5);
+            this.panel_tratamientos.Controls.Add(this.pictureBox2);
             this.panel_tratamientos.Controls.Add(this.tratamiento_btn_Modificar);
             this.panel_tratamientos.Controls.Add(this.tratamiento_btn_Inhabilitar);
             this.panel_tratamientos.Controls.Add(this.tratamiento_tbx_Descripcion);
             this.panel_tratamientos.Controls.Add(this.tratamiento_tbx_Nombre_Tratamiento);
             this.panel_tratamientos.Controls.Add(this.tratamiento_btn_Registrar);
-            this.panel_tratamientos.Controls.Add(this.pictureBox1);
             this.panel_tratamientos.Controls.Add(this.tratamientos_dgv);
             this.panel_tratamientos.Controls.Add(this.label4);
             this.panel_tratamientos.Controls.Add(this.label3);
             this.panel_tratamientos.Controls.Add(this.label2);
-            this.panel_tratamientos.Controls.Add(this.label1);
-            this.panel_tratamientos.Controls.Add(this.pictureBox2);
+            this.panel_tratamientos.Controls.Add(this.pictureBox3);
             this.panel_tratamientos.Location = new System.Drawing.Point(-4, -3);
             this.panel_tratamientos.Name = "panel_tratamientos";
             this.panel_tratamientos.Size = new System.Drawing.Size(810, 539);
@@ -74,11 +74,11 @@
             // 
             // tratamiento_btn_Modificar
             // 
-            this.tratamiento_btn_Modificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(241)))), ((int)(((byte)(244)))));
+            this.tratamiento_btn_Modificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(169)))), ((int)(((byte)(234)))));
             this.tratamiento_btn_Modificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tratamiento_btn_Modificar.Location = new System.Drawing.Point(569, 327);
+            this.tratamiento_btn_Modificar.Location = new System.Drawing.Point(593, 382);
             this.tratamiento_btn_Modificar.Name = "tratamiento_btn_Modificar";
-            this.tratamiento_btn_Modificar.Size = new System.Drawing.Size(133, 51);
+            this.tratamiento_btn_Modificar.Size = new System.Drawing.Size(133, 38);
             this.tratamiento_btn_Modificar.TabIndex = 15;
             this.tratamiento_btn_Modificar.Text = "Modificar";
             this.tratamiento_btn_Modificar.UseVisualStyleBackColor = false;
@@ -86,52 +86,42 @@
             // 
             // tratamiento_btn_Inhabilitar
             // 
-            this.tratamiento_btn_Inhabilitar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(241)))), ((int)(((byte)(244)))));
+            this.tratamiento_btn_Inhabilitar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(60)))), ((int)(((byte)(44)))));
             this.tratamiento_btn_Inhabilitar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tratamiento_btn_Inhabilitar.Location = new System.Drawing.Point(569, 417);
+            this.tratamiento_btn_Inhabilitar.Location = new System.Drawing.Point(593, 437);
             this.tratamiento_btn_Inhabilitar.Name = "tratamiento_btn_Inhabilitar";
-            this.tratamiento_btn_Inhabilitar.Size = new System.Drawing.Size(133, 51);
+            this.tratamiento_btn_Inhabilitar.Size = new System.Drawing.Size(133, 38);
             this.tratamiento_btn_Inhabilitar.TabIndex = 14;
             this.tratamiento_btn_Inhabilitar.Text = "Inhabilitar";
             this.tratamiento_btn_Inhabilitar.UseVisualStyleBackColor = false;
             // 
             // tratamiento_tbx_Descripcion
             // 
-            this.tratamiento_tbx_Descripcion.Location = new System.Drawing.Point(297, 167);
+            this.tratamiento_tbx_Descripcion.BackColor = System.Drawing.Color.AliceBlue;
+            this.tratamiento_tbx_Descripcion.Location = new System.Drawing.Point(298, 197);
             this.tratamiento_tbx_Descripcion.Name = "tratamiento_tbx_Descripcion";
             this.tratamiento_tbx_Descripcion.Size = new System.Drawing.Size(222, 22);
             this.tratamiento_tbx_Descripcion.TabIndex = 13;
             // 
             // tratamiento_tbx_Nombre_Tratamiento
             // 
-            this.tratamiento_tbx_Nombre_Tratamiento.Location = new System.Drawing.Point(56, 167);
+            this.tratamiento_tbx_Nombre_Tratamiento.BackColor = System.Drawing.Color.AliceBlue;
+            this.tratamiento_tbx_Nombre_Tratamiento.Location = new System.Drawing.Point(57, 197);
             this.tratamiento_tbx_Nombre_Tratamiento.Name = "tratamiento_tbx_Nombre_Tratamiento";
             this.tratamiento_tbx_Nombre_Tratamiento.Size = new System.Drawing.Size(205, 22);
             this.tratamiento_tbx_Nombre_Tratamiento.TabIndex = 12;
             // 
             // tratamiento_btn_Registrar
             // 
-            this.tratamiento_btn_Registrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(241)))), ((int)(((byte)(244)))));
+            this.tratamiento_btn_Registrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(192)))), ((int)(((byte)(229)))));
             this.tratamiento_btn_Registrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tratamiento_btn_Registrar.Location = new System.Drawing.Point(569, 138);
+            this.tratamiento_btn_Registrar.Location = new System.Drawing.Point(593, 181);
             this.tratamiento_btn_Registrar.Name = "tratamiento_btn_Registrar";
-            this.tratamiento_btn_Registrar.Size = new System.Drawing.Size(133, 51);
+            this.tratamiento_btn_Registrar.Size = new System.Drawing.Size(133, 38);
             this.tratamiento_btn_Registrar.TabIndex = 11;
             this.tratamiento_btn_Registrar.Text = "Registrar";
             this.tratamiento_btn_Registrar.UseVisualStyleBackColor = false;
             this.tratamiento_btn_Registrar.Click += new System.EventHandler(this.tratamiento_btn_Registrar_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(620, 15);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(174, 44);
-            this.pictureBox1.TabIndex = 8;
-            this.pictureBox1.TabStop = false;
             // 
             // tratamientos_dgv
             // 
@@ -144,61 +134,9 @@
             this.tratamientos_dgv.Name = "tratamientos_dgv";
             this.tratamientos_dgv.RowHeadersWidth = 51;
             this.tratamientos_dgv.RowTemplate.Height = 24;
-            this.tratamientos_dgv.Size = new System.Drawing.Size(463, 185);
+            this.tratamientos_dgv.Size = new System.Drawing.Size(464, 185);
             this.tratamientos_dgv.TabIndex = 7;
             this.tratamientos_dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tratamientos_dgv_CellContentClick);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(51, 258);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(409, 38);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "TRATAMIENTOS DISPONIBLES";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(301, 138);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(98, 18);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Descripcion";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(52, 138);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(190, 18);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Nombre del Tratamiento";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(49, 42);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(412, 38);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "REGISTRO DE TRATAMIENTOS";
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
-            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(807, 103);
-            this.pictureBox2.TabIndex = 10;
-            this.pictureBox2.TabStop = false;
             // 
             // idTratamiento
             // 
@@ -221,6 +159,69 @@
             this.Descripcion.Name = "Descripcion";
             this.Descripcion.Width = 125;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(53, 258);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(398, 34);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "TRATAMIENTOS DISPONIBLES";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(302, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Descripción";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre del Tratamiento";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(1686, 778);
+            this.pictureBox3.TabIndex = 20;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(41, 20);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(172, 51);
+            this.pictureBox2.TabIndex = 21;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(406, 41);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(320, 45);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "TRATAMIENTOS";
+            // 
             // form_tratamientos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -232,8 +233,8 @@
             this.Load += new System.EventHandler(this.form_tratamientos_Load);
             this.panel_tratamientos.ResumeLayout(false);
             this.panel_tratamientos.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tratamientos_dgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
@@ -247,15 +248,15 @@
         private System.Windows.Forms.TextBox tratamiento_tbx_Descripcion;
         private System.Windows.Forms.TextBox tratamiento_tbx_Nombre_Tratamiento;
         private System.Windows.Forms.Button tratamiento_btn_Registrar;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.DataGridView tratamientos_dgv;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.DataGridViewTextBoxColumn idTratamiento;
         private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
         private System.Windows.Forms.DataGridViewTextBoxColumn Descripcion;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label5;
     }
 }
